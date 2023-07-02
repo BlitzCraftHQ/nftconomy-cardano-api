9530991645f2e4a48c4bf6ddf8cc64e96f9694be
Fabian Ferno <fabianferno@gmail.com>
2023-07-03 03:53:10 +0530

    Remove redundant

--- src/controllers/mints/mints.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import Controller from "./mints.controller";
-
-const mints: Router = Router();
-const controller = new Controller();
-
-mints.get("/avg-mints-per-wallet", controller.GetAvgMintsPerWallet);
-
-export default mints;
